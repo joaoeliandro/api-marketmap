c63edb22fdf9bd59632966df121a94435afa6ae5
João Eliandro <eliandrogermano@gmail.com>
2020-08-23 20:39:06 -0300

    feat: implemented route

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const Marketmap = require('./function');
 
 const app = express();
 
-const data = path.resolve(__dirname, 'database', 'dataset.json');
+const data = path.resolve(__dirname, 'database', 'data-example.json');
 
 app.get('/products-route', (request, response) => {
   const marketmap = new Marketmap(data);
 
-  return response.json(marketmap.getProducts());
+  return response.json(marketmap.getRoutes());
 });
 
 app.listen(3333, () => {
